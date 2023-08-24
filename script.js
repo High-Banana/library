4d321e8338b9f58d4e11d1f990d001102a6fc3f2
Potassium <brainiacgamer444@gmail.com>
2023-08-24 14:05:24 +0545

    Make function to get book details from user

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const myLibrary = [];
+const tableContainer = document.querySelector(".table-container");
+const bookTitle = document.getElementById("book_name");
+const authorName = document.getElementById("author_name");
+const totalPage = document.getElementById("pages");
+const radioButton = document.getElementsByName("status");
+const submitButton = document.querySelector(".submit-button");
+let bookStatus;
 
 function Book(author, title, pages, status){
     this.author = author,
@@ -7,8 +14,24 @@ function Book(author, title, pages, status){
     this.status = status
 }
 
-function addBookToLibrary(){
-    
+function getRadioButtonValue(){
+    for(let i=0;i<radioButton.length;i++){
+        if(radioButton[i].checked){
+            bookStatus = radioButton[i].value;
+        }
+    }
 }
 
+function addBookToLibrary(){
+    submitButton.addEventListener("click", (event)=>{
+        getRadioButtonValue();
+        event.preventDefault();
+        const book = new Book(authorName.value, bookTitle.value, totalPage.value, bookStatus);
+        myLibrary.push(book);
+    })
+}
 addBookToLibrary();
+
+function createTable(){
+
+}
