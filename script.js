d0059e08fa6ca2cc57277dea5d924ffc406d0cd8
Potassium <brainiacgamer444@gmail.com>
2023-08-24 19:29:23 +0545

    Make function to add button inside table's last cell

--- script.js
+++ script.js
@@ -13,22 +13,27 @@ let bookTitle;
 let authorName;
 let NumOfPage;
 let bookStatus;
-const header = ["Name", "Author", "No. of pages", "Status"];
+let libraryAction = "";
+const header = ["Name", "Author", "No. of pages", "Status", "Action"];
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages, status, libraryAction) {
     this.title = title
     this.author = author
     this.pages = pages
     this.status = status
+    this.libraryAction = libraryAction
 }
 
 function addBookToLibrary() {
     submitButton.addEventListener("click", (event) => {
         event.preventDefault();
         getBook();
-        const book = new Book(bookTitle, authorName, NumOfPage, bookStatus);
+        getRadioButtonValue();
+        // if(bookTitle === "" || authorName === "" || NumOfPage === "" || bookStatus === "") return;
+        const book = new Book(bookTitle, authorName, NumOfPage, bookStatus, libraryAction);
         myLibrary.unshift(book);
         displayBook();
+        addButtonInsideCell();
     })
 }
 addBookToLibrary();
@@ -50,6 +55,7 @@ function getRadioButtonValue() {
 function displayBook() {
     tableContainer.textContent = "";
     const table = document.createElement("table");
+    table.classList.add("custom-table");
     const tableRow = document.createElement("tr");
 
     // Make table row and table header
@@ -87,6 +93,17 @@ function hideForm() {
     addNewBookButton.style.display = "block";
 }
 
+function addButtonInsideCell() {
+    const table = document.querySelector(".custom-table");
+    let rows = table.rows;
+    for(let i = 1; i < rows.length; i++){
+        let cols = rows[i].cells;
+        let lastCol = rows[i].cells[cols.length-1];
+        let button = document.createElement("button");
+        button.textContent = "Remove";
+        lastCol.appendChild(button);
+    }
+}
 window.addEventListener("load", hideForm);
 addNewBookButton.addEventListener("click", displayForm);
 closeFormButton.addEventListener("click", hideForm);
